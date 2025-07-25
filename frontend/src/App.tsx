7b597bdb3b9ee9dca2cbd95d90af0459b84c321c
jaimemoransantos <jaimemoransantos@gmail.com>
2025-07-25 04:18:19 -0500

    chore: add layout comment to App for desktop-first design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,13 @@ import UsersMap from "./components/UsersMap";
 
 function App() {
   return (
+    // This page distribution is set for desktop as project will run on desktop
+    // Will need to adjust for mobile
+
     <div className="h-screen flex overflow-hidden ">
       <div className="flex-1 w-full relative bg-gray-100 h-full ">
         <UsersMap />
       </div>
-
-      {/* Sidebar - 1/3 of screen */}
       <div className="p-4 w-1/3 bg-white border-l border-gray-200 flex flex-col min-w-0 gap-3">
         <UsersList />
       </div>
