3f2d6a3092941a1d1594993cebad55326396da83
jaimemoransantos <jaimemoransantos@gmail.com>
2025-07-28 01:58:26 -0500

    refactor: set ZIP code as standard

--- frontend/src/components/ui/ListItem.tsx
+++ frontend/src/components/ui/ListItem.tsx
@@ -11,9 +11,7 @@ const ListItem = ({
   setIsEditModalOpen: (isOpen: boolean) => void;
 }) => {
   const handleClick = (user: User | null) => {
-    console.log("user", user);
     if (user) {
-      console.log("user2", user);
       setSelectedUser(user);
       setIsEditModalOpen(true);
     }
@@ -27,7 +25,7 @@ const ListItem = ({
       <div className="flex gap-2 w-full justify-between">
         <div className="flex flex-col justify-between">
           <p>{user?.name}</p>
-          <p>Zip Code: {user?.zipCode}</p>
+          <p>ZIP Code: {user?.zipCode}</p>
         </div>
         <div className="flex flex-col gap-2 justify-between">
           <p className="self-end">{formatUtcOffset(user?.timezone || 0)}</p>
