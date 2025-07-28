6364db4a1cbf73180d1e1f80c0fa3085a76e2b1d
jaimemoransantos <jaimemoransantos@gmail.com>
2025-07-28 02:24:25 -0500

    refactor: improve List Item style

--- frontend/src/components/ui/ListItem.tsx
+++ frontend/src/components/ui/ListItem.tsx
@@ -22,18 +22,28 @@ const ListItem = ({
       className="flex border p-2 border-gray-200 rounded-lg hover:bg-gray-100 cursor-pointer"
       onClick={() => handleClick(user)}
     >
-      <div className="flex gap-2 w-full justify-between">
+      <div className="flex gap-4 w-full justify-between">
         <div className="flex flex-col justify-between">
-          <p>{user?.name}</p>
-          <p>ZIP Code: {user?.zipCode}</p>
+          <div>
+            <span className="text-gray-500 text-sm">Name</span>
+            <p className="text-base">{user.name}</p>
+          </div>
+          <div>
+            <span className="text-gray-500 text-sm">ZIP Code</span>
+            <p className="text-base">{user.zipCode}</p>
+          </div>
         </div>
-        <div className="flex flex-col gap-2 justify-between">
-          <p className="self-end">
-            Timezone: {formatUtcOffset(user?.timezone || 0)}
-          </p>
-          <p className="self-end">
-            Coordinates: {user?.latitude},{user?.longitude}
-          </p>
+        <div className="flex flex-col justify-between items-end text-right">
+          <div>
+            <span className="text-gray-500 text-sm">Timezone</span>
+            <p className="text-base">{formatUtcOffset(user.timezone)}</p>
+          </div>
+          <div>
+            <span className="text-gray-500 text-sm">Coordinates</span>
+            <p className="text-base">
+              {user.latitude}, {user.longitude}
+            </p>
+          </div>
         </div>
       </div>
     </div>
