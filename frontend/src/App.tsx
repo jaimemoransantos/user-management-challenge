5326dfa70f59f3564bd9e863bdf6593da1de04c5
jaimemoransantos <jaimemoransantos@gmail.com>
2025-07-28 01:54:23 -0500

    refactor: use semantic HTML elements

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,14 +7,14 @@ function App() {
     // This page distribution is set for desktop as project will run on desktop
     // Will need to adjust for mobile
 
-    <div className="h-screen flex overflow-hidden ">
+    <main className="h-screen flex overflow-hidden ">
       <div className="flex-1 w-full relative bg-gray-100 h-full ">
         <UsersMap />
       </div>
-      <div className="p-4 w-1/3 bg-white border-l border-gray-200 flex flex-col min-w-0 gap-3">
+      <div className="p-4 w-1/3 bg-white border-l border-gray-200 flex flex-col min-w-0 gap-3 h-full overflow-y-auto">
         <UsersList />
       </div>
-    </div>
+    </main>
   );
 }
 
